20e3087eab721461f8853adfbd4192f50a34ed05
Juuso Vilmunen <juuso@vilmunen.net>
2018-05-03 20:30:25 +0300

    changing username

--- src/identity.vue
+++ src/identity.vue
@@ -1,18 +1,30 @@
 <template>
 	<div>
-		<h2>Identity</h2>
+		<div class="row">
+			<div class="col-sm-12">
+				<h2>Identity</h2>
+				
+				<form id="profile">
+				  <div class="form-group">
+				    <label class="control-label col-sm-2" >Username:</label>
+				    <input v-model="profile.name" type="text"  />
+				  </div>
+				  <button v-on:click="apply">Apply</button>
+				</form>
+			</div>
+		</div>
 		
-		<p>
-			Profile name {{ profile.name }}
-		
-		    <img class="id-img" :src="profile.thumbnail" :alt="profile.name">
-			<h1>{{ profile.name }}</h1>
-		    <h2 class="id-publickey">Your ID</h2>
-			<p>{{ identityid }}</p>
-		
-		    <h3 class="id-publickey">Your public key</h3>
-			<pre>{{ viewpublickeystring }}</pre>
-		</p>
+		<div class="row">	
+			<div class="col-sm-12">		
+			    <img class="id-img" :src="profile.thumbnail" :alt="profile.name">
+				<h1>{{ profile.name }}</h1>
+			    <h2 class="id-publickey">Your ID</h2>
+				<p>{{ identityid }}</p>
+			
+			    <h3 class="id-publickey">Your public key</h3>
+				<pre>{{ viewpublickeystring }}</pre>
+			</div>
+		</div>	
 	</div>	
 </template>
 
@@ -22,6 +34,12 @@ import {getIdentity, getSecret} from './modules/identity';
 export default {
 	data() {
 		return getIdentity().data;
+	},
+	methods: {
+		apply: function(e) {
+			console.log("got e " + e);
+			console.log("profile name " + getIdentity().save());
+		}
 	}
 }
 
